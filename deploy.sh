78c288b1115f3bf4edfabc1ad55c97c0cb975bc1
Axel Froborg <axel.froborg@gmail.com>
2023-02-22 17:48:37 +0100

    IT DEPLOYS

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,4 @@
 #!/bin/bash -e
 
-TARGET=aarch64-unknown-linux-gnu
-USER=pi
-
-# --- Uncomment this to build the client ---
-
-# # Build the html files
-# cd client
-# pnpm run build
-
-# # Go back home
-# cd ..
-
-# --- To Here ---
-
-# Build the server
-cd server
-cargo install -f cross
-cross build --target $TARGET --release
-
-# TODO: Copy the binary to the target
-# TODO: Execute the binary on the target
+sh ./scripts/deploy-pi.sh
+sh ./scripts/deploy-frontend.sh
