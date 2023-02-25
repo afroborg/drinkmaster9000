e52bf21a3c0d6b6ed93507e890e267a13f691445
Axel Froborg <axel.froborg@gmail.com>
2023-02-25 16:50:14 +0100

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,50 @@
 #!/bin/bash -e
 
+readonly NAME=drinkmixer-pi
+readonly TARGET_HOST=pi@pi
+readonly TARGET_PATH=/srv/drinkmixer
+readonly TARGET_ARCH=aarch64-unknown-linux-gnu
+
+readonly CLIENT_SOURCE_PATH=dist/
+readonly SERVER_SOURCE_PATH=target/${TARGET_ARCH}/release/${NAME}
+
+
 # check that the first argument is either frontend or pi, or that it is empty
 if [[ $1 == "frontend" ]] || [[ -z $1 ]]; then
-    sh ./scripts/deploy-frontend.sh
+
+    # remove the static folder on the pi
+    ssh -t ${TARGET_HOST} rm -rf ${TARGET_PATH}/static
+
+    cd client
+
+    # build the frontend
+    pnpm run build
+
+    # copy the static folder to the pi
+    rsync -r ${CLIENT_SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}/static
+    
+    cd ..
 fi
 
 if [[ $1 == "pi" ]] || [[ -z $1 ]]; then
-    sh ./scripts/deploy-pi.sh
+    # Script from https://medium.com/swlh/compiling-rust-for-raspberry-pi-arm-922b55dbb050
+
+    set -o errexit
+    set -o nounset
+    set -o pipefail
+    set -o xtrace
+
+    cd pi
+
+    # build the server
+    cargo build --target ${TARGET_ARCH} --release
+
+    # copy the server to the pi
+    rsync ${SERVER_SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+
+    # copy the config to the pi
+    rsync config.ron ${TARGET_HOST}:${TARGET_PATH}
+
+    # restart the service
+    ssh -t ${TARGET_HOST} sudo systemctl restart drinkmixer.service
 fi
